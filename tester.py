e9e2244d316e4acdaaf629f8537a55f437e53a28
dzaky.fattan <70198829+DzakyFattan@users.noreply.github.com>
2023-05-21 13:38:24 +0700

    Revert "fix: stopping thread mechanism"
    This reverts commit 37d21da22afd23a2f59d9c1204ae5282a57fbe9d.

--- tester.py
+++ tester.py
@@ -1,17 +1,10 @@
 import subprocess
 import signal
 import time
-import os
 
-# from typing import List
-# test: List[str] = []
-# print(test)
-
-# Define the command to open a new terminal window
-if os.name == 'nt':  # for Windows
-    CMD_NEW_TERMINAL = 'start cmd /k'
-else:  # for Linux and macOS
-    CMD_NEW_TERMINAL = 'x-terminal-emulator -e'
+from typing import List
+test: List[str] = []
+print(test)
 
 # generate 5 servers
 server_scripts = [
@@ -20,51 +13,58 @@ server_scripts = [
         "py": "server.py",
         "args": ["localhost", "2401"],
     },
-    # # follower
-    # {
-    #     "py": "server.py",
-    #     "args": ["localhost", "2402", "localhost", "2401"],
-    # },
-    # # follower
-    # {
-    #     "py": "server.py",
-    #     "args": ["localhost", "2403", "localhost", "2401"],
-    # },
-    # # follower
-    # {
-    #     "py": "server.py",
-    #     "args": ["localhost", "2404", "localhost", "2401"],
-    # },
-    # # follower
-    # {
-    #     "py": "server.py",
-    #     "args": ["localhost", "2405", "localhost", "2401"],
-    # },
+    # follower
+    {
+        "py": "server.py",
+        "args": ["localhost", "2402", "localhost", "2401"],
+    },
+    # follower
+    {
+        "py": "server.py",
+        "args": ["localhost", "2403", "localhost", "2401"],
+    },
+    # follower
+    {
+        "py": "server.py",
+        "args": ["localhost", "2404", "localhost", "2401"],
+    },
+    # follower
+    {
+        "py": "server.py",
+        "args": ["localhost", "2405", "localhost", "2401"],
+    },
 ]
 
 client_scripts = [
-    # # client to leader
-    # {
-    #     "py": "client.py",
-    #     "args": ["localhost", "2431", "localhost", "2401"],
-    # },
-    # # client to follower
-    # {
-    #     "py": "client.py",
-    #     "args": ["localhost", "2432", "localhost", "2402"],
-    # },
+    # client to leader
+    {
+        "py": "client.py",
+        "args": ["localhost", "2431", "localhost", "2401"],
+    },
+    # client to follower
+    {
+        "py": "client.py",
+        "args": ["localhost", "2432", "localhost", "2402"],
+    },
 ]
 
 # start servers
-server_processes = []
 for server in server_scripts:
-    cmd = CMD_NEW_TERMINAL + f' python {server["py"]} ' + ' '.join(server["args"])
-    server_processes.append(subprocess.Popen(cmd, shell=True))
+    process = subprocess.Popen(["python", server["py"]] + server["args"])
+    server["process"] = process
     time.sleep(1)
 
 # start clients
-client_processes = []
 for client in client_scripts:
-    cmd = CMD_NEW_TERMINAL + f' python {client["py"]} ' + ' '.join(client["args"])
-    client_processes.append(subprocess.Popen(cmd, shell=True))
+    process = subprocess.Popen(["python", client["py"]] + client["args"])
+    client["process"] = process
     time.sleep(1)
+
+time.sleep(5)
+
+# Kill a server and its clients
+server_to_kill = server_scripts[0]
+clients_to_kill = [client for client in client_scripts if client["args"][3] == server_to_kill["args"][1]]
+for client in clients_to_kill:
+    client["process"].send_signal(signal.SIGINT)
+server_to_kill["process"].send_signal(signal.SIGINT)
